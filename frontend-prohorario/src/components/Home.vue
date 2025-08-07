8579e3bc83161d6dec34880cdfed3c54652df651
paiva737 <rafaelpaiva636@gmail.com>
2025-08-07 20:26:53 -0300

    adicionando rota de pagina principal e login

---
+++ frontend-prohorario/src/components/Home.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="text-center mt-10 text-2xl font-bold">Página Inicial</div>
+</template>
--- frontend-prohorario/src/main.js
+++ frontend-prohorario/src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import router from './router'
 import './assets/main.css'
 
-
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
